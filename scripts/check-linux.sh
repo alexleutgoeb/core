de01d1775035c70bda9407ae33fdcc78f47cb215
Alex Leutgöb <privat@alexleutgoeb.com>
2016-05-26 18:30:28 +0200

    Use number instead of true

--- scripts/check-linux.sh
+++ scripts/check-linux.sh
@@ -28,7 +28,7 @@ missing_deps=""
 # Check parameters
 while [ -n "$1" ]; do
   case $1 in
-    --install-dependencies )    install_deps=true
+    --install-dependencies )    install_deps=1
                                 ;;
   esac
   shift
@@ -43,7 +43,7 @@ done
 
 if [ -n "$missing_deps" ]; then
   # Check if we should auto-install dependencies
-  if [ "$install_deps" -eq true ]; then
+  if [ "$install_deps" -eq 1 ]; then
     # Uses sudo command, may result in a user prompt
     sudo apt-get update -qq
     sudo apt-get -y install$missing_deps
